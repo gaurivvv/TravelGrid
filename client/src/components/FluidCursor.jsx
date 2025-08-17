81cd0a0e92bc33551182900bd1457c22d75add25
Adarsh Chaubey <adarsh03chaubey@gmail.com>
2025-08-17 23:23:45 +0530

    Revert "Fix: Fluid Cursor Overlapping UI Elements (UI/UX Improvement)"

--- client/src/components/FluidCursor.jsx
+++ client/src/components/FluidCursor.jsx
@@ -8,11 +8,8 @@ const FluidCursor = () => {
   }, []);
 
   return (
-    <div
-      className="fixed top-0 left-0 w-full h-full z-0 pointer-events-none"
-      style={{ inset: 0 }}
-    >
-      <canvas id="fluid" className="w-full h-full" />
+    <div className="fixed top-0 left-0 z-2 pointer-events-none">
+      <canvas id="fluid" className="w-screen h-screen" />
     </div>
   );
 };
