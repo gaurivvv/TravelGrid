75721d113148b60721715faff49760feeb7ddee5
imprince26 <princepatel260806@gmail.com>
2025-08-31 09:31:30 +0530

    chore(ui): change bg of guides card

--- client/src/components/Custom/CustomCarousel.jsx
+++ client/src/components/Custom/CustomCarousel.jsx
@@ -96,8 +96,8 @@ const CustomCarousel = ({ guides, viewprofilehandle, isHome = false }) => {
             <div
               key={index}
               className={`${position} w-[280px] sm:w-[300px] h-[400px] flex items-center justify-center text-center flex-col hover:-translate-y-[10px] backdrop-blur-md rounded-2xl p-6 ${isDarkMode
-                ? "bg-white/10 border border-white/20 hover:border-white/40"
-                : "bg-white/80 border border-gray-200 hover:border-pink-300"
+                ? "bg-black/30 border border-white/20 hover:border-white/40"
+                : "bg-white/30 border border-gray-300 hover:border-pink-300"
                 } ${position === "center" ? "scale-100" : "scale-90 opacity-80"} card`}
             >
               <div className="card-image">
